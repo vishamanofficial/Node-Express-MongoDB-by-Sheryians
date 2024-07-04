5561fe54f95aa8f5ea3b0cf8cae8555ce0a727fa
Aman Vishwakarma <vishamanofficial@gmail.com>
2024-07-04 09:05:03 +0530

    mern

--- L01/script.js
+++ L01/script.js
@@ -5,15 +5,34 @@
 // for each, map, filter, find, indexof
 
 var arr = [1,2,3,4,];
- 
+// forEach 
 var arr2 = arr.forEach((val)=>{
     return val + " hello"
 });
 console.log(arr2);
-
+// map 
 var arr3 = arr.map((val)=>{
     return val + " hello"
 });
 console.log(arr3);
+// find
+var arr4 = arr.find((val)=>{
+    if (val === 2) return val;
+});
+console.log(arr4);
+// indexOf 
+var arr5 = arr.indexOf(2)
+console.log(arr5);
 
 
+// obejcts 
+var obj = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+
+// function 
+function abcd(){
+    return 12;
+}
